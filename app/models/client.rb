7b7ff677c706dd78197006d69c2773db857d96ab
Rolando <rolando@rolando-VGN-NR160E.(none)>
2012-11-10 06:00:29 -0700

    Segundo commit

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,4 @@
 class Client < ActiveRecord::Base
   attr_accessible :apellido_materno, :apellido_paterno, :calle, :ciudad_id, :email, :estado_id, :nombre, :numero_exterior, :numero_interior
+  has_many :orders
 end
